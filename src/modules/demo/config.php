cb52c564903e1b1427cc573d959d2d12a0292a10
David E <me@davidjeddy.com>
2015-09-28 16:12:08 -0400

    REPLACED GLOBALLY {TAB} with 4* {SPACE}

--- src/modules/demo/config.php
+++ src/modules/demo/config.php
@@ -4,15 +4,16 @@ use humhub\modules\dashboard\widgets\Sidebar;
 
 return [
     'class' => 'humhub\modules\demo\Module',
-    'events' => [
+    'events' => [ 
+        // ref: https://www.humhub.org/docs/guide-dev-module-events.html
         [
-        	'class' => Sidebar::className(),
-        	'callback' => [
-        		'humhub\modules\demo\Module', 'onSidebarInit'
-        	],
-        	'event' => Sidebar::EVENT_INIT,
-    	],
+            'class'    => Sidebar::className(),
+            'callback' => [
+                'humhub\modules\demo\Module', 'onSidebarInit'
+            ],
+            'event' => Sidebar::EVENT_INIT,
+        ],
     ],
-	'id'        => 'demo',
-	'namespace' => 'humhub\modules\demo'
+    'id'        => 'demo',
+    'namespace' => 'humhub\modules\demo'
 ];
